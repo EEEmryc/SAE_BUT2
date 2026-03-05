00d634daded6fd444cf2f2fbab9b5da801a811fe
Emryc :) <emrychumeau@gmail.com>
2026-03-05 20:16:13 +0100

    Update UserRepository.java

--- src/main/java/sae/learnhub/learnhub/domain/repository/UserRepository.java
+++ src/main/java/sae/learnhub/learnhub/domain/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package sae.learnhub.learnhub.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sae.learnhub.learnhub.domain.model.User;
+import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
