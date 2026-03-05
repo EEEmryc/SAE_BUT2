ce7e990411b5244c15d681afa5342d6e2570c673
Emryc :) <emrychumeau@gmail.com>
2026-03-05 20:53:34 +0100

    ajout des DTO ForgotPasswordRequest et ResetPasswordRequest

---
+++ src/main/java/sae/learnhub/learnhub/domain/dto/ForgotPasswordRequest.java
@@ -0,0 +1,8 @@
+package sae.learnhub.learnhub.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class ForgotPasswordRequest {
+    private String email;
+}
---
+++ src/main/java/sae/learnhub/learnhub/domain/dto/ResetPasswordRequest.java
@@ -0,0 +1,9 @@
+package sae.learnhub.learnhub.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class ResetPasswordRequest {
+    private String token;
+    private String newPassword;
+}
