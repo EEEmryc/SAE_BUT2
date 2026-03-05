1f22878fe70c7383a2ff3575de0bb361cc5ec221
Emryc :) <emrychumeau@gmail.com>
2026-03-05 20:13:07 +0100

    Adaptation de user.java a la bdd

--- src/main/java/sae/learnhub/learnhub/domain/model/User.java
+++ src/main/java/sae/learnhub/learnhub/domain/model/User.java
@@ -2,6 +2,7 @@ package sae.learnhub.learnhub.domain.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "utilisateur")
@@ -11,12 +12,25 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String nom;
+
+    @Column(nullable = false, length = 100)
     private String prenom;
-    @Column(unique = true)
+
+    @Column(unique = true, nullable = false, length = 255)
     private String email;
-    @Column(name="mot_de_passe")
+
+    @Column(name = "mot_de_passe", nullable = false, length = 255)
     private String password;
-    private String role;
+
+    @Column(nullable = false, length = 20)
+    private String role; 
+
+    @Column(length = 50)
     private String statut;
+
+    @Column(name = "date_creation", updatable = false)
+    private LocalDateTime dateCreation = LocalDateTime.now();
 }
