e4ecb5c9674c7c4a84a5b873a33465a8f3b9e5dd
Emryc :) <emrychumeau@gmail.com>
2026-03-05 20:34:19 +0100

    test: ajout des tests unitaires pour valider la sécurité de AdminController

---
+++ src/test/java/sae/learnhub/learnhub/api/controller/AdminControllerTest.java
@@ -0,0 +1,32 @@
+package sae.learnhub.learnhub.api.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AdminControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnForbiddenWhenUserIsNotAdmin() throws Exception {
+        mockMvc.perform(get("/api/admin/users"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMINISTRATEUR")
+    void shouldReturnOkWhenUserIsAdmin() throws Exception {
+        mockMvc.perform(get("/api/admin/users"))
+                .andExpect(status().isOk());
+    }
+}
