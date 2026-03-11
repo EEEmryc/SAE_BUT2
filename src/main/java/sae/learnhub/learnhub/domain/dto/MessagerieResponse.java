6f3b4c9aa32349cb9c65ed307b8b22ecee584407
Yacine AIT HAMI <yacine@valars.com>
2026-03-11 17:11:35 +0100

    - Messagerei Tester :

---
+++ src/main/java/sae/learnhub/learnhub/domain/dto/MessagerieResponse.java
@@ -0,0 +1,32 @@
+package sae.learnhub.learnhub.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessagerieResponse {
+
+    private Long id;
+    private String sujet;
+    private String contenu;
+    private LocalDateTime dateEnvoi;
+    private Boolean lu;
+    private LocalDateTime dateLecture;
+
+    // Sender info
+    private Long expediteurId;
+    private String expediteurNom;
+    private String expediteurPrenom;
+    private String expediteurEmail;
+
+    // Receiver info
+    private Long destinataireId;
+    private String destinataireNom;
+    private String destinatairePrenom;
+    private String destinataireEmail;
+}
