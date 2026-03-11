a85cd7db16e4df7f38f19d31fb7e087abce9b539
Yacine AIT HAMI <yacine@valars.com>
2026-03-11 20:52:55 +0100

    - Delete FIc Password for the Docker ( Just for Debuging)

--- Docker/fix-passwords.sql
+++
@@ -1,4 +0,0 @@
--- Fix BCrypt password hashes using proper format
-UPDATE utilisateur SET mot_de_passe = '$2a$10$N.zmdr9k7shNzBsNqEqTLOHrZ8tUUDQW1Gq3k1FeJfUxqF5wjT5X.' WHERE email = 'admin@learnhub.com';
-UPDATE utilisateur SET mot_de_passe = '$2a$10$N.zmdr9k7shNzBsNqEqTLOHrZ8tUUDQW1Gq3k1FeJfUxqF5wjT5X.' WHERE email = 'prof@learnhub.com';
-UPDATE utilisateur SET mot_de_passe = '$2a$10$N.zmdr9k7shNzBsNqEqTLOHrZ8tUUDQW1Gq3k1FeJfUxqF5wjT5X.' WHERE email = 'student@learnhub.com';
