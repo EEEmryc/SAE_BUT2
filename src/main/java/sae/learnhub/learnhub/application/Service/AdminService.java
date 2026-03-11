24f81c13cf8bc19524d4865c45f64799f046a533
Saber <157073174+SaberBcf@users.noreply.github.com>
2026-03-11 19:48:41 +0100

    Implémentation du AdminService pour le calcul des statistiques

---
+++ src/main/java/sae/learnhub/learnhub/application/Service/AdminService.java
@@ -0,0 +1,21 @@
+package sae.learnhub.learnhub.application.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sae.learnhub.learnhub.domain.dto.StatsResponse;
+import sae.learnhub.learnhub.domain.repository.CoursRepository;
+import sae.learnhub.learnhub.domain.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+    private final UserRepository userRepository;
+    private final CoursRepository coursRepository;
+
+    public StatsResponse getGlobalStatistics() {
+        long totalUsers = userRepository.count();
+        long activeCourses = coursRepository.countByStatut("PUBLISHED");
+        return new StatsResponse(totalUsers, activeCourses);
+    }
+}
